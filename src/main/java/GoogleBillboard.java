f7ff884f0540965a0c4a56f79df0733d1523987c
napham1 <143123637+napham1@users.noreply.github.com>
2024-01-16 11:11:38 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -5,7 +5,7 @@ public void setup()
       String digits = e.substring(i,i+10);
       double num = Double.parseDouble(digits);
       if(isPrime(num) == true){
-        System.out.print(Double.toString(num));
+        System.out.print(num);
         break;
       }
     }
