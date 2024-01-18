484a6948975f509d848327ec1e6824e356ad2f1b
napham1 <143123637+napham1@users.noreply.github.com>
2024-01-17 20:23:02 -0800

    Update BillboardTester.java

--- src/test/java/BillboardTester.java
+++ src/test/java/BillboardTester.java
@@ -21,7 +21,7 @@ public class BillboardTester {
      GoogleBillboard.main(null);
 
      // assertion
-     assertEquals("7.427466391E9"\n, bos.toString()); 
+     assertEquals("7.427466391E9\n", bos.toString()); 
 
      // undo the binding in System
      System.setOut(originalOut);
